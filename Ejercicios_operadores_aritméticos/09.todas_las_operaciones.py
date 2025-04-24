cadab3d8fb6858e79cb22c7f2c0a4f7d56c98813
Vanesa Carrillo <vcmvanesamedina7@gmail.com>
2025-04-24 18:27:45 -0500

    Update 09.todas_las_operaciones.py

--- Ejercicios_operadores_aritméticos/09.todas_las_operaciones.py
+++ Ejercicios_operadores_aritméticos/09.todas_las_operaciones.py
@@ -2,7 +2,7 @@
 #Pide al usuario dos números y muestra: suma, resta, multiplicación y división, separadas por coma.
 
 #Pedir dos números al ususario
-print ("Para conocer la suma, resta, multiplicación y división de dos números, ingresa dos números: ")
+print ("Conocer la suma, resta, multiplicación y división de dos números")
 num1 = float(input("Ingresa el primer número  "))
 num2 = float(input("Ingresa el segundo número  "))
 
